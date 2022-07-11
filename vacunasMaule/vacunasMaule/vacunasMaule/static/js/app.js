028c9924a9bb0f7da5ee6f328b7b0425a98d567a
NatyNatur <naticcg@gmail.com>
2022-07-11 17:50:46 -0400

    validaciones pacientes

--- vacunasMaule/vacunasMaule/vacunasMaule/static/js/app.js
+++ vacunasMaule/vacunasMaule/vacunasMaule/static/js/app.js
@@ -1,3 +1,85 @@
 function limpiarForm() {
     document.formularioIngreso.reset();
 }
+
+function ingresarPaciente() {
+    var rut = document.formularioIngreso.rut.value;
+    var nombre = document.formularioIngreso.nombre.value;
+    var appaterno = document.formularioIngreso.appaterno.value;
+    var apmaterno = document.formularioIngreso.apmaterno.value;
+    var edad = document.formularioIngreso.edad.value;
+    var vacuna = document.formularioIngreso.vacuna.value;
+    var fecha = document.formularioIngreso.fecha.value;
+
+    if (rut.length < 8 || rut.length > 11 || rut.indexOf('-') === -1) {
+        alert('Debe ingresar un rut válido con guión.')
+        document.formularioIngreso.rut.focus();
+        return false;
+    }
+
+    if (nombre.length < 3) {
+        alert('Debe ingresar un nombre.');
+        document.formularioIngreso.nombre.focus();
+        return false;
+    }
+
+    if (appaterno.length < 2) {
+        alert('Debe ingresar un apellido paterno.');
+        document.formularioIngreso.appaterno.focus();
+        return false;
+    }
+
+    if (apmaterno.length < 2) {
+        alert('Debe ingresar un apellido materno.');
+        document.formularioIngreso.materno.focus();
+        return false;
+    }
+
+    if (edad <= 0 || edad === '') {
+        alert('Debe ingresar edad del paciente.');
+        document.formularioIngreso.edad.focus();
+        return false;
+    }
+
+    if (vacuna.length < 2) {
+        alert('Debe ingresar el nombre de la vacuna.');
+        document.formularioIngreso.vacuna.focus();
+        return false;
+    }
+
+    if (fecha.length < 2) {
+        alert('Debe ingresar una fecha válida.');
+        document.formularioIngreso.fecha.focus();
+        return false;
+    }
+
+    document.formularioIngreso.action = "/ingresar_registro";
+    document.formularioIngreso.submit() = true;
+}
+
+function buscarPaciente() {
+    var rut = document.formularioBusqueda.rut_paciente.value;
+    
+    if (rut.length < 8 || rut.length > 11 || rut.indexOf('-') === -1) {
+        alert('Debe ingresar un rut válido con guión.')
+        document.formularioBusqueda.rut_paciente.focus();
+        return false;
+    }
+    
+    document.formularioBusqueda.action = "/busca_pte";
+    document.formularioBusqueda.submit() = true;
+}
+
+function eliminarPaciente() {
+    var rut = document.formularioEliminacion.rut_paciente.value;
+    
+    if (rut.length < 8 || rut.length > 11 || rut.indexOf('-') === -1) {
+        alert('Debe ingresar un rut válido con guión.')
+        document.formularioEliminacion.rut_paciente.focus();
+        return false;
+    }
+    
+    document.formularioEliminacion.action = "/elimina_pte";
+    document.formularioEliminacion.submit() = true;
+}
+
